cccfe47287467891612b93048a0c53b1c22c2209
StreamMao <maosijun1995@gmail.com>
2020-05-12 13:22:02 -0400

    Passport facebook OAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1112740362437798',
+        clientSecret: '9305e85b2f903f486e7f35185e2fc42e'
+    }
 }
